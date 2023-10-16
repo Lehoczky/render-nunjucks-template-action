5d7a9aae7c6f329c08d5b2c18ffaf64981baac65
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2023-10-16 23:38:40 +0200

    test: mock github runner environmental variable to pass tests on CI

--- jest.config.mjs
+++ jest.config.mjs
@@ -14,6 +14,7 @@ const config = {
   },
   coverageReporters: ["json-summary", "text", "lcov"],
   collectCoverageFrom: ["./src/**"],
+  setupFiles: ["<rootDir>/jest.setup.js"],
 }
 
 export default config
